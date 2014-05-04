3c8d994a67265665481fa1f90520c219e5bef1f3
Luiscarlo Rivera <luiscarlo6@gmail.com>
2014-05-04 13:58:52 -0430

    Implementadas funciones para la nueva firma de Pixels

--- Haskell/P2/Pixels.hs
+++ Haskell/P2/Pixels.hs
@@ -1,6 +1,30 @@
-import qualified Data.Map
-import qualified Graphics.HGL
+import qualified Data.Map as DM
+import qualified Graphics.HGL as G
+import qualified Data.Maybe as M
 
-data Pixels = Pixels { color ::  Graphics.HGL.Color, dots ::[[Pixel]] }
+data Pixels = Pixels { color :: G.Color, dots ::[[Pixel]] } deriving Show
 
-data Pixel = Pixel { on :: Bool }
+data Pixel = Pixel { on :: Bool } deriving (Show)
+
+font :: DM.Map Char Pixels -> Char -> Pixels
+font m c = pix $ DM.lookup c m
+          where pix may = if M.isJust may then M.fromJust may
+                          else Pixels G.White [[]]
+
+oldPixelsToPixels :: [String] -> Pixels
+oldPixelsToPixels s = Pixels G.White $ map (\st -> map (\x -> if x ==' ' then Pixel False else Pixel True) st) s
+
+
+pixelListToPixels :: [Pixels]->Pixels
+pixelListToPixels lp = foldr (\p1 p2 -> Pixels G.White (dots p1 ++ [[]] ++dots p2)) (Pixels G.White [[]]) lp
+
+concatPixels :: [Pixels] -> Pixels
+concatPixels lp = foldr (\ p1 p2 -> Pixels G.White (zipWith' (++) (dots p1) (dots p2))) (Pixels G.White [[]]) lp
+                  where zipWith' f a b = if null (head b) then a else zipWith (++) a b
+
+
+--variables de uso 
+a = oldPixelsToPixels [" *** ","*   *","*   *","*   *","*****","*   *","*   *"]
+b = oldPixelsToPixels ["**** ","*   *","*   *","**** ","*   *","*   *","**** "]
+c = [a,b]
+
