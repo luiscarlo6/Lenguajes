85b4f04590d977ea4ff2ecd1b9338606dc7dd1c5
Jose Julian <eljjpr416@hotmail.com>
2014-05-06 09:29:29 -0430

    Mudado el font

--- Haskell/P2/led-display.hs
+++ Haskell/P2/led-display.hs
@@ -2,16 +2,10 @@ import qualified Data.List as DL
 import qualified Data.Char as DC
 import qualified System.Environment as SE (getArgs)
 import qualified System.IO as SIO 
-import qualified Data.Map as M
+import qualified Data.Map as DM
 import Pixels
 
 mensajeLC = "\nError: Deben haber al menos dos archivos en la linea de comandos\n"
--- mensajeAV = "\nError: El archivo font esta vacio\n"
--- mensajeNN = "\nError: Los numeros suministrados en el archivo deben de ser positivos\n"
--- mensajeNC = "\nError: Los tamaños de filas y columnas no corresponden\n"
--- mensajeMC = "\nError: Hay un caracter mas entre comillas\n"
-
-
 
 
 main = do
@@ -24,81 +18,62 @@ main = do
             x <- readFont fontEntrada1
             print ( x)
             --print "\n"
---             fontEntrada <- SIO.hGetContents fontEntrada1
- {-   
-            --caso archivo font vacio
-            if DL.null fontEntrada 
-              then putStrLn mensajeAV
-                  
-              else do let numeros = obtenerNumeros (DL.head (DL.lines fontEntrada)) 
-                                    
-                      if not (DL.all (>0) numeros)
-                        then putStrLn mensajeNN
-                        
-                        else do let contenidoFont = readFont(fontEntrada)
-                                --print numeros
-                                --print contenidoFont
-                                if not (validarTamaños (fromEnum(last numeros)) (fromEnum(head numeros)) contenidoFont ) 
-                                  then putStrLn mensajeNC
-                                  else if not (validarUnicidad contenidoFont) 
-                                        then putStrLn mensajeMC
-                                        else do let final = map (\(x,y)-> ((stringToChar x),y)) contenidoFont
+
+
+readFont :: SIO.Handle -> IO (DM.Map Char Pixels)
+readFont n = do fontEntrada <- SIO.hGetContents n
+                     --caso archivo font vacio
+                if DL.null fontEntrada 
+                    then error mensajeAV
+                    
+                    else do let numeros = obtenerNumeros (DL.head (DL.lines fontEntrada)) 
+                            --valido que todos los numeros sean degativos
+                            if not (DL.all (>=0) numeros)
+                              then error mensajeNN
+                              
+                              else do let contenidoFont = temporal(fontEntrada)
                                           
-                                                print (M.fromList final)-}
+                                      if not (validarTam (fromEnum(last numeros)) (fromEnum(head numeros)) contenidoFont ) 
+                                          then error mensajeNC
+                                          else if not (validarUnicidad contenidoFont) 
+                                              then error mensajeMC
+                                              else do let final = map (\(x,y)-> ((head x),y)) contenidoFont
+                                                      return (DM.fromList(map (\(x,y)-> (x,(oldPixelsToPixels y)))final))
+  where
+    --mensajes de errores
+    mensajeAV = "\nError: El archivo font esta vacio\n"
+    mensajeNN = "\nError: Los numeros suministrados en el archivo deben de ser positivos\n"
+    mensajeNC = "\nError: Los tamaños de filas y columnas no corresponden\n"
+    mensajeMC = "\nError: Hay un caracter mas entre comillas\n"
+  
+    --Valida que halla exactamente un caracter entre comillas
+    validarUnicidad :: [([a], t)] -> Bool
+    validarUnicidad cont = all (\(x,y)-> (DL.length x) == 1) cont
+                                      
+                                      
+    --valida que el tamaño que representa los pixeles se corresponda
+    validarTam :: Int -> Int -> [(t, [[a]])] -> Bool
+    validarTam fil colm cont =  all (\(w,x)-> (length x == fil) && (all (\w -> (length w == colm)) x) ) cont
 
 
-                                              
-                                              
--- salida n = do fontEntrada <- SIO.hGetContents n
---               let alicia = readFont (fontEntrada)
---                   beth = map (\(x,y)-> ((stringToChar x),y)) alicia
---               return (M.fromList(map (\(x,y)-> (x,(oldPixelsToPixels y)))beth))
---                                                 
---                                                 
---                                                 
---                                                 
--- validarUnicidad cont = all (\(x,y)-> (DL.length x) == 1) cont
---                                   
---                                   
--- --valida que el tamaño que representa los pixeles se corresponda
--- validarTamaños fil colm cont =  all (\(w,x)-> (length x == fil) && (all (\w -> (length w == colm)) x) ) cont
--- 
--- 
--- --obtengo los numeros que traen en el archivo
--- obtenerNumeros n = map stringToInt (words n)
--- 
--- --se le pasa el archivo font leido y lo procesa
--- readFont n = map (\(x,y)->(x,fst y))(procesar (read (last (words (head (lines n ))))::Int) ((dropWhile null $ tail $ lines n )))
--- 
--- -- procesa el font
--- procesar num [] = []
--- procesar num ls = ((eliminarEspeciales(DL.head b)),DL.splitAt num (DL.tail b) ) : procesar num (DL.dropWhile DL.null bs)
---   where (b,bs) = DL.break null ls
---         
--- 
---         
--- eliminarEspeciales n = (DL.delete  '\"' (DL.delete  '\"' n))--(\(x:_)->x)
--- 
--- 
--- stringToChar n = (\(x:_)->x)n
--- 
--- 
--- 
--- 
--- --convertir un string numerico en numero
--- stringToInt n = if DL.head n == '-' 
---                 then (-1)*(convertirNum(map (DC.digitToInt) (tail n))) 
---                 else convertirNum(map (DC.digitToInt) n)
---   where
---     --Llamo a convertirNumero pero con un solo argumento
---     convertirNum n = convertirNumero 0 n
--- 
---     --convertir una lista de enteros en su numeor equivalente ejemplo [1,0] = 10
---     convertirNumero n x = if DL.null x 
---                           then n 
---                           else convertirNumero (n +(((potenciaDiez ((DL.length x)-1) 1 )*(DL.head x)))) (DL.tail x)
--- 
---     --Obtengo la n-sima potencia de diez
---     potenciaDiez n r = if n == 0 
---                       then r 
---                       else potenciaDiez (n-1)(r * 10) 
+    --obtengo los numeros que traen en el archivo
+    --Y valido que no vengan cosas tipo a1... pero se escapan --1
+
+    obtenerNumeros :: String -> [Int]
+    obtenerNumeros n = if all (\x -> all (\y-> DC.isDigit y || y == '-') x)(words n)
+                      then (map (\x -> read x::Int) (words n))
+                      else error "Numero no validos"
+
+    --se le pasa el archivo font leido y lo procesa
+    temporal :: String -> [([Char], [[Char]])]
+    temporal n = map (\(x,y)->(x,fst y))(obtenerTuplas (read (last (words (head (lines n ))))::Int) ((dropWhile null $ tail $ lines n )))
+
+    -- procesa el font
+    obtenerTuplas :: Int -> [[Char]] -> [([Char], ([[Char]], [[Char]]))]
+    obtenerTuplas num [] = []
+    obtenerTuplas num ls = ((eliminarEspeciales(DL.head b)),DL.splitAt num (DL.tail b) ) : obtenerTuplas num (DL.dropWhile DL.null bs)
+      where (b,bs) = DL.break null ls
+            
+    --eliminino los \" que vienen del archivo
+    eliminarEspeciales :: [Char] -> [Char]
+    eliminarEspeciales n = (DL.delete  '\"' (DL.delete  '\"' n))
