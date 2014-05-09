2865082a0eb7445c9ef7378f6ea7e1a022e403a4
Jose Julian <eljjpr416@hotmail.com>
2014-05-09 00:30:06 -0430

    Implementada funcion que procesa todo el documento y comentado lo que faltaba

--- Haskell/P2/led-display.hs
+++ Haskell/P2/led-display.hs
@@ -1,3 +1,14 @@
+{-| 
+  Module      : led-display.hs
+  Copyright   : Universidad Simón Bolívar
+  Maintainer  : Luiscarlo Rivera (09-11020) 
+                & José Julián Prado (09-11006)
+                & Grupo 9 Taller de Lenguajes de Programación I (CI-3661) 
+                Entrega: Proyecto # 2
+                 
+  conteniendo el codigo fuente Haskell para el programa principal
+-}
+
 import qualified System.Environment as SE (getArgs)
 import qualified System.IO as SIO 
 import qualified Data.List as DL
@@ -6,6 +17,7 @@ import Effects
 
 mensajeLC = "\nError: Deben haber al menos dos archivos en la linea de comandos\n"
 
+
 main = do
   archivos <- SE.getArgs
 
@@ -13,47 +25,15 @@ main = do
     then error mensajeLC --Caso error en la linea de comandos
         
     else do fontEntrada1 <- SIO.openFile (DL.head archivos) SIO.ReadMode
-            effects1 <- SIO.openFile (DL.head $ DL.tail archivos) SIO.ReadMode
             x <- readFont fontEntrada1
-            y <- readDisplayInfo effects1
-			-- procesarArchivos (DL.tail archivos)  x
+            y <- procesarArchivos (DL.tail archivos) []
             ledDisplay x y
 
 
-
-
-
-
-            
-           
-  
-
--- procesarArchivos [] x      = putStrLn "Bye!"
--- procesarArchivos (fn:fns) x = do 
---                                   putStrLn $ "Processing " ++ fn
---                                   effects1 <- SIO.openFile (fn) SIO.ReadMode
---                                   y <- readDisplayInfo effects1
---                                   
---                                   
---                                   print y
--- 
---                                   let p =  hacerPantalla $ dots $ font x 'B'
---                                   print p
---                                   let g = map dibujarPixel
---                                   G.runGraphics $ do
---                                   w <- G.openWindowEx
---                                         "Pixels"
---                                         Nothing
---                                         (800, 600)
---                                         G.DoubleBuffered
---                                         (Just 50)
---                                   G.clearWindow w
---                                   
---                                   G.setGraphic w $ G.overGraphics $ map dibujarPixel $ hacerPantalla $ dots $ messageToPixels x "De nada"
---                                   --G.setGraphic w $ dibujarPixel (1,0)
---                                   G.getWindowTick w
---                                   G.getKey w
---                                   G.closeWindow w
---                                   
--- 
---                                   procesarArchivos fns x
+-- | Procesa todos los archivos y retorna una lista con todos los efectos
+procesarArchivos :: [FilePath] -> [Effects] -> IO [Effects]
+procesarArchivos []  n    = return n
+procesarArchivos (x:xs) n = do effects1 <- SIO.openFile (x) SIO.ReadMode
+                               y <- readDisplayInfo effects1
+                               procesarArchivos xs (n++y)
+                                
