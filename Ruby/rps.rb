f3760468cf85d5825d4aaccd27c8ee0d97c37bab
Jose Julian <eljjpr416@hotmail.com>
2014-06-21 20:24:35 -0430

    Falta implementar los next

--- Ruby/rps.rb
+++ Ruby/rps.rb
@@ -66,45 +66,103 @@ class Strategy
 end
 
 class Uniform < Strategy
+  attr_accessor :estrategia, :original
+  
+  def initialize lista
+    unless lista.empty?
+      @estrategia = lista.uniq
+      @original = lista.uniq
+    else
+      raise "Error: La Lista en Uniform no puede ser vacia"
+    end
+  end
+  
   def next m
   end
 
   def to_s
+    @aux = self.class.name + " ["
+    @estrategia.each {|x|@aux = @aux + x.to_s + ", "}
+    @aux = @aux.chomp(", ") + " ]"
   end
 
   def reset
+    @estrategia = @original
   end
 end
 
 class Biased < Strategy
+  attr_accessor :estrategia, :original,:probabilidad
+  
+   def initialize mapa
+    unless mapa.empty?
+      @estrategia = mapa
+      @original = mapa.clone
+      @probabilidad = cal_prob
+    else
+      raise "Error: El mapa no puede estar vacio"
+    end
+  end
+  
   def next m
   end
 
   def to_s
+    @aux = self.class.name + " {"
+    @estrategia.each {|key, value| @aux = @aux + ":#{key} => #{value/@probabilidad.to_f}, " }
+    @aux = @aux.chomp(", ") + " }" 
   end
 
   def reset
+    @estrategia = @original
+  end
+  
+  def cal_prob
+    @prob = 0
+    @estrategia.values.each {|x| @prob = @prob + x}
+    @prob
   end
 end
 
 class Mirror < Strategy
+  attr_accessor :estrategia, :original
+  
+  def initialize mov
+    @estrategia = mov
+    @original = mov
+  end
+  
   def next m
   end
 
   def to_s
+    @aux = self.class.name + "  Movimiento Inicial = " + @estrategia.to_s 
   end
 
   def reset
+    @estrategia = @original
   end
 end
 
 class Smart < Strategy
+  attr_accessor :p, :r, :s
+  
+  def initialize
+    @p = 0
+    @r = 0
+    @s = 0 
+  end
+  
   def next m
   end
 
   def to_s
+    @aux = self.class.name + "  p = " + @p.to_s + ", r = " + @r.to_s + ", s = " + @s.to_s 
   end
 
   def reset
+    @p = 0
+    @r = 0
+    @s = 0 
   end
 end
